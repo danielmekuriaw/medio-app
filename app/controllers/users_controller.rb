3562ee1b23adafbb3b8435c10bdb2aeecac8f15a
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-07-14 13:52:07 -0500

    Added time validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,21 @@ class UsersController < ApplicationController
     end
 
     def create
+        byebug
+        @user = User.create(user_params)
 
+        if @user.valid?
+          @user.save
+          redirect_to user_path(@user)
+        else
+          render :new
+        end
+    end
+
+
+    private
+
+    def user_params
+        #params.require(:user).permit(:)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,10 @@ class User < ApplicationRecord
     #Validations
 
     validates :username, presence: true, uniqueness: true
-    validates :bio, presence: true
-    #validates :age,
+    validates :bio, presence: true, length: { minimum: 100 }
+    validates :age, numericality: {greater_than: 0, less_than: 120, only_integer: true}
+    
+    validates_date :birth_date, on_or_before: lambda { Date.current }
+
     #validates :email, presence: true, uniqueness: true
 end
