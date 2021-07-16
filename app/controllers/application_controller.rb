d2972ae54850de374e6f0fd5db1ae1bfa0e85f41
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-07-15 20:15:42 -0500

    Added the login authentication and the navigation bar - along with its dependency on who the current user is

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   gmaps = GoogleMapsService::Client.new(key: 'AIzaSyAmCf0SWHM3vMqJi6oCkLkPaRq3GJ57tis')
+
+  include ApplicationHelper
+
+  private
+
+  def redirect_if_not_logged_in
+    redirect_to login_path if !logged_in?
+  end
   
 end
